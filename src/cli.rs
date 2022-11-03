76b4356f156270bb8c7e8cb37765ef543ac3bfcd
leon3s <leo.vernisse@gmail.com>
2022-11-03 16:48:22 +0100

    [FEATURE] Man page generation

---
+++ src/cli.rs
@@ -0,0 +1,11 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about=None)]
+pub(crate) struct Cli {
+    #[arg(short, long="force-choose", help="Force runfast to choose a new runner, instead of \
+        looking for one that may already be set")]
+    pub(crate) force_choose_new: bool,
+    #[arg(short, long="config-path", help="Load specific toml config", default_value="~/.config/runfast/runners.toml")]
+    pub(crate) config_path: String,
+}
